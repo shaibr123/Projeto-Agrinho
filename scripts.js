50927c65622aa5a7f4e8447d58a63e6bc0fca687
Shaiane Rodrigues <130679811+shaibr123@users.noreply.github.com>
2024-06-03 08:41:29 -0300

    organizando

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const mensagem = document.getElementById('mensagem').value;
+
+        if (nome && email && mensagem) {
+            alert('Mensagem enviada com sucesso!');
+            form.reset();
+        } else {
+            alert('Por favor, preencha todos os campos.');
+        }
+    });
+});
